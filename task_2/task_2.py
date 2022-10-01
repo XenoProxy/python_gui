bc3b330f41714203dac00771fea68fd7226e43f8
XenoProxy <ksenomorph6@gmail.com>
2022-10-01 23:45:42 +0300

    first commit

---
+++ task_2/task_2.py
@@ -0,0 +1,43 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+import sys
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("Simple programm")
+        self.setGeometry(300, 250, 350, 200)
+
+        self.new_text = QtWidgets.QLabel(self)
+
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("Just sample text")
+        self.main_text.move(100, 100)
+        self.main_text.adjustSize()
+
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(70, 150)
+        self.btn.setText("Push me")
+        self.btn.setFixedWidth(200)
+        self.btn.clicked.connect(self.add_label)
+
+    def add_label(self):
+        self.new_text.setText("Second text")
+        self.new_text.move(100, 50)
+        self.new_text.adjustSize()
+
+
+def application():
+    app = QApplication(sys.argv)
+    window = Window()    
+
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    application()
+    
